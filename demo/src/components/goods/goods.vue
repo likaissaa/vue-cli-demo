b276baa73d0d120295eb4db0d0f1ded62f514ea2
likaissaa <644786607@qq.com>
2018-05-25 17:58:56 +0800

    less+goods 组建
    less 尝试使用——goods 组件完善

--- demo/src/components/goods/goods.vue
+++ demo/src/components/goods/goods.vue
@@ -1,14 +1,76 @@
 <template>
 	<div class="goods">
 		<div class="menu-wrapper">
-			1
+			<ul>
+				<li v-for="item in goods" class="menu-item">
+					<span class="text">
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+					</span>
+				</li>
+				
+			</ul>
 		</div>
 		<div class="foods-wrapper">
-			1
+			<ul>
+				<li v-for="item in goods">
+					<h1 class="title">{{item.name}}</h1>
+					<ul>
+						<li v-for="food in item.foods" class="food-item border-1px">
+							<div class="icon">
+								<img width="57" height="57" :src="food.icon" />
+							</div>
+							<div class="content">
+								<h2 class="name">{{food.name}}</h2>
+								<p class="desc">{{food.description}}</p>
+								<div class="extra">
+									<span class="count">月售{{food.sellCount}}份</span>
+									<span>好评率{{food.rating}}%</span>
+								</div>
+								<div class="price">
+									<span class="now">￥{{food.price}}</span>
+									<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
+<script>
+const ERR_OK =0;
+export default{
+	//定义以为常量
+	
+	props:{
+		seller:{
+			type:Object
+		}
+	},
+	data(){
+		return{
+			goods:[]
+		};
+	},
+	created(){
+		this.classMap=['decrease','discount','special','invoice','guarantee'];
+		
+	   this.axios.get("/api/goods")
+        .then(response => {
+        	if(response.data.errno==ERR_OK){
+        		this.goods=response.data.data
+        		console.log(this.goods)
+        	}
+        	else{
+        		alert("失败")
+        	}
+        })
+	}
+	}
+</script>
 <style scoped lang="less">
+@import "../../common/less/mixin.less";
 	.goods{
 		position:absolute;
 		width:100%;
@@ -19,11 +81,105 @@
 		.menu-wrapper{
 			flex:0 0 80px;
 			width:80px;
-			background-color:red;
-		}
+			background-color:#f5f5f5;
+			.menu-item{
+				display:table;
+				height:54px;
+				width:56px;
+				line-height:14px;
+				font-size:12px;
+				padding:0 12px;	
+				.icon{
+					display: inline-block;
+					vertical-align: top;
+					width: 12px;
+					height: 12px;
+					margin-right: 2px;
+					background-size: 12px 12px;
+					background-repeat: no-repeat;
+					&.decrease{
+						.bg-image('goods/decrease_3');}
+					&.discount{
+						.bg-image('goods/discount_3');}
+					&.guarantee{
+						.bg-image('goods/guarantee_3');}
+					&.invoice{
+						.bg-image('goods/invoice_3');}
+					&.special{
+						.bg-image('goods/special_3');}
+				}
+				.text{
+					display:table-cell;
+					font-size:12px;
+					width:56px;
+					vertical-align:middle;
+					.border-1px(rgba(7,17,27,0.1))
+				}
+			}
+			}
 		.foods-wrapper{
 			flex:1;
+			.title{
+				padding-left:14px;
+				height:26px;
+				line-height:26px;
+				border-left:2px solid #d9dde1;
+				font-size:12px;
+				color:rgb(147,153,159);
+				background:#f3f5f7;
+			}
+			.food-item{
+				display:flex;
+				margin:18px;
+				padding-bottom:18px;
+				.border-1px(rgba(7,17,27,0.1));
+				&:last-child{
+					.border-none;
+					margin-bottom:0;
+				}
+				.icon{
+					flex:0 0 57px;
+					margin-right: 10px;
+					}
+				.content{
+					flex:1;
+					.name{
+						margin:2px 0 8px 0;
+						height:14px;
+						line-height:14px;
+						font-size:14px;
+						color:rgb(7,17,27);
+					}
+					.desc,.extra{
+						line-height:10px;
+						font-size:10px;
+						color:rgb(147,153,159);
+					}
+					.desc{
+						margin-bottom:8px;
+					}
+					.extra{
+						&.count{
+							margin-right:12px
+						}
+						
+					}
+					.price{
+						font-weight:700;
+						line-height:24px;
+						.now{
+							margin-right:18px;
+							font-size:14px;
+							color:rgb(240,20,20);
+						}
+						.old{
+							text-decoration:line-through;
+							font-size: 10px;
+							color:rgb(147,153,159);
+						}
+					}
+				}
+			}
 		}
-		
 	}
 </style>
